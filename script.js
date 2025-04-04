48af65042182f6eff9b70c3684a367e663d19acc
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-04 16:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit',function (event) {
+
+	event.preventDefault();
+	const age=document.getElementById('age').value;
+	const name=document.getElementById('name').value;
+	if(age === '' || name === '')
+	{
+		alert('Please enter valid details');
+		return;
+	}
+	new Promise((resolve,reject) => {
+		if (parseInt(age) >= 18) {
+			setTimeout(() =>{
+				resolve();
+			},4000);
+		} else {
+			reject();
+		}
+	}).then(() => {
+		alert('Welcome, . You can vote.');
+	}).catch(() =>{
+		alert('Oh sorry . You aren't old enough.')
+	});
+	
+});
