6f0cc449f6a2e3985b4a28ed3402dbfdb255aea5
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-04 20:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,26 +4,24 @@ document.getElementById('myForm').addEventListener('submit',function (event) {
 	event.preventDefault();
 	const age=document.getElementById('age').value;
 	const name=document.getElementById('name').value;
-	if(age === '' || name === '')
+	if(!age || !name)
 	{
 		alert('Please enter valid details');
 		return;
 	}
 	new Promise((resolve,reject) => {
-		if (parseInt(age) >= 18) {
-			
+		
 			setTimeout(() =>{
-				resolve();
-			},4000);
+				
+				if(age >= 18)
+				resolve('Welcome,'+name+ '.You can vote.');
 		}
+					   
 		else {
-			reject();
+			reject('Oh sorry' +name+''. You aren't old enough.');
 		}
-	}).then(() => {
-		alert('Welcome,'+name+ '.You can vote.');
-	}).catch(() =>{
-		alert('Oh sorry' +name+''. You aren't old enough.')
-	});
-}
-	
+	},4000);
+})
+.then(message => alert(message));
+.catch(message => alert(message));
 });
