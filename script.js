6142c14f063eefbd83748df5c2137582a2180599
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-04 21:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//your JS code here. If required.
-document.getElementById('myForm').addEventListener('submit',function (event) {
-	
-	event.preventDefault();
-	const age=document.getElementById('age').value;
-	const name=document.getElementById('name').value;
-	if(!age || !name)
-	{
-		alert('Please enter valid details');
-		return;
-	}
-	new Promise((resolve,reject) => {
-	
-			setTimeout(() => {
-				
-				if(age >= 18)
-				resolve('Welcome,'+name+ '.You can vote.');
-		}
-					   
-		else {
-			reject('Oh sorry' +name+''. You aren't old enough.');
-		}
-	},4000);
-})
-.then(message => alert(message))
-.catch(message => alert(message));
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const age = document.getElementById('age').value;
+    const name = document.getElementById('name').value;
+    if (!age || !name) {
+        alert('Please enter valid details');
+        return;
+    }
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age >= 18) {
+                resolve('Welcome, ' + name + '. You can vote.');
+            } else {
+                reject('Oh sorry ' + name + '. You aren\'t old enough.');
+            }
+        }, 4000);
+    })
+    .then(message => alert(message))
+    .catch(message => alert(message));
 });
